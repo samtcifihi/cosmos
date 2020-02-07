c2f452d14013c362a07328c4bcae35daf11693b9
samtcifihi <la.lojban@gmail.com>
2020-02-07 17:44:24 -0500

    Comment Overview of Purpose

--- src/main.go
+++ src/main.go
@@ -1,4 +1,28 @@
 package main
 
 func main() {
+	// Create a world where a small number of agents make choices,
+	// some determined by random numbers from random.org.
+	// This simulates a cosmos.
+	// Let the simulation be stepwise advanced by the user.
+	// The user has all knowledge of the cosmos
+	// and can predict some of what will happen
+	// by observing probable results of the random decision points.
+	// However, the user does not know for certain
+	// Everything about what choices will be made
+	// By the agents,
+	// as that knowledge does not yet exist.
+	// Hence can a logically omnicient Being have things
+	// which are not known to them,
+	// because they are not logically possible to know;
+	// or,
+	// more specifically,
+	// because said Being created a cosmos
+	// (although he could have chosen not to)
+	// in which there were choices
+	// whose outcome could not--
+	// or more properly,
+	// chose not to change the cosmological properties to allow to--
+	// be determined
+	// before their resolution.
 }
